73c9183196b66601c144f37061565100499f4535
Mario Ćelić <mariocelic@gmail.com>
2022-10-04 10:35:30 +0200

    Finished new application and added solution to project

--- src/Bookstore.Service/Program.cs
+++ src/Bookstore.Service/Program.cs
@@ -1,3 +1,5 @@
+using Rhetos;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,13 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddRhetosHost((serviceProvider, rhetosHostBuilder) => rhetosHostBuilder
+        .ConfigureRhetosAppDefaults()
+        .UseBuilderLogProviderFromHost(serviceProvider)
+        .ConfigureConfiguration(cfg => cfg.MapNetCoreConfiguration(builder.Configuration)))
+    .AddAspNetCoreIdentityUser()
+    .AddHostLogging();
 
 var app = builder.Build();
 
